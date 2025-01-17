551b23e7e4ddc3bb12cd8b8d30cd03224d131f50
Ramonprog <programador.ramon@gmail.com>
2025-01-17 18:12:10 -0300

    feat: configurar banco de dados com Flask-Migrate e atualizar dependências

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_migrate import Migrate
+from db import db
 
 app = Flask(__name__)
 
@@ -6,6 +8,18 @@ app = Flask(__name__)
 def hello():
   return 'Api is live'
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/books'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate = Migrate(app, db)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080, host='0.0.0.0')
+
+
+"""
+comandos para iniciar o banco de dados
+flask db init
+flask db migrate
+"""
